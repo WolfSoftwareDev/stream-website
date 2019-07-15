8e26f4d1394eaf752110c81ffb424e01496cbb07
WolfSoftwareDev <43505755+WolfSoftwareDev@users.noreply.github.com>
2019-07-15 03:51:55 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: process.env.MongoURI || 'mongodb+srv://stream:stream-site@wsd-main-0-3fndz.mongodb.net/TwitchSite?retryWrites=true&w=majority'
+    MongoURI: process.env.MongoURI || 'mongodb+srv://<username>:<password>@wsd-main-0-3fndz.mongodb.net/<database>?retryWrites=true&w=majority'
 }
